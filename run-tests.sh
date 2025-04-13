ad5311eb28a2c96f2ca153b4a0238d9ef503c7f8
TzerNanLinStudio <tzernan.lin.studio@gmail.com>
2025-04-13 16:01:48 +0800

    test2

--- run-tests.sh
+++ run-tests.sh
@@ -4,5 +4,14 @@ set -e
 # 構建 Docker 映像
 docker build -t my-app .
 
-# 運行 Docker 容器，格式化代碼並執行測試
-docker run --rm -v $(pwd):/app my-app sh -c "black . && pytest"
+# 檢查代碼格式
+docker run --rm -v $(pwd):/app my-app sh -c "black . --check"
+if [ $? -ne 0 ]; then
+  echo "Code not formatted correctly. Please run black."
+  # 格式化代碼
+  docker run --rm -v $(pwd):/app my-app sh -c "black ."
+  exit 1
+fi
+
+# 格式正確時運行測試
+docker run --rm -v $(pwd):/app my-app sh -c "pytest"

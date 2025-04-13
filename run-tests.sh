acf492a85abcd63cf283a762c9d4ac2889d4cc93
TzerNanLinStudio <tzernan.lin.studio@gmail.com>
2025-04-13 16:20:24 +0800

    test2

--- run-tests.sh
+++ run-tests.sh
@@ -5,13 +5,18 @@ set -e
 docker build -t my-app .
 
 # 檢查代碼格式
-docker run --rm -v $(pwd):/app my-app sh -c "black . --check"
-if [ $? -ne 0 ]; then
-  echo "Code not formatted correctly. Please run black."
+if ! docker run --rm -v $(pwd):/app my-app sh -c "black . --check"; then
+  echo "Code not formatted correctly. Formatting now..."
   # 格式化代碼
   docker run --rm -v $(pwd):/app my-app sh -c "black ."
+  echo "\033[0;31mCode formatted. Please commit the changes. Tests aborted.\033[0m"
+  # 刪除 Docker 映像
+  docker rmi my-app
   exit 1
 fi
 
 # 格式正確時運行測試
 docker run --rm -v $(pwd):/app my-app sh -c "pytest"
+
+# 測試完成後刪除 Docker 映像
+docker rmi my-app
--- tests/test_vision.py
+++ tests/test_vision.py
@@ -1,6 +1,6 @@
 def test_basic_assertion():
     assert 1 + 1 == 2
 
-def test_basic_assertion2():
 
-    assert 10    + 10         == 20
+def test_basic_assertion2():
+    assert 10 + 10    == 20
